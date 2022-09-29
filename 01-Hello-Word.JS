395c612d2617bb3f7bccdcfd7f8ac931209f3eb2
Gabriel Nogueira <nogueira.melogabriel@gmail.com>
2022-09-29 19:54:25 -0300

    criado arquivos

---
+++ 01-Hello-Word.JS
@@ -0,0 +1,2 @@
+console.log("Hello Word, JavaScript")
+console.log("SejaDev")
---
+++ 02-variaveis.js
@@ -0,0 +1,14 @@
+const x = 10
+let number = 5 
+let string = "amo voce"
+
+
+const dias = 365
+const qtMeses = (dias / 30) 
+const qtDias = (qtMeses * 30)
+const qtAnos = (qtMeses/12)
+
+console.log(qtAnos)
+
+
+
